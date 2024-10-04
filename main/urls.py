18beca4e6946c9fb785b7d70f99b35400b8d2003
AndhikaNayakaAW <andhikanayaka@gmail.com>
2024-10-04 10:19:22 +0700

    pushing pws

---
+++ main/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_mood, delete_mood, add_mood_entry_ajax
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', show_main, name='show_main'),
+    path('create-restaurant-entry', create_mood_entry, name='create_restaurant_entry'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('edit-restaurant/<uuid:id>', edit_restaurant, name='edit_restaurant'),
+    path('delete/<uuid:id>', delete_restaurant, name='delete_restaurant'), # adjust to the name of the function you created
+    path('create-restaurant-entry-ajax', add_restaurant_entry_ajax, name='add_restaurant_entry_ajax'),
+]
