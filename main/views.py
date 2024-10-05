02e55dcc6486e18f75bd80db5c276a55d6db2eac
adyoprawira <adyoprawira@gmail.com>
2024-10-05 15:11:05 +0700

    Added register, login, logout, and last logout feature

--- main/views.py
+++ main/views.py
@@ -1,22 +1,25 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.urls import reverse
 from main.models import Restaurants
 from main.forms import RestoEntryForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+import datetime
+from django.urls import reverse
 
+
+@login_required(login_url='/login')
 def show_main(request):
     resto_entries = Restaurants.objects.all()
 
     context = {
-        'resto_entries': resto_entries
+        'resto_entries': resto_entries,
+        'last_login': request.COOKIES.get('last_login', 'Not set'),
     }
 
     return render(request, "main.html", context)
@@ -58,3 +61,38 @@ def delete_restaurant(request, id):
     item = Restaurants.objects.get(pk = id)
     item.delete()
     return HttpResponseRedirect(reverse('main:show_main'))
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
