8016e7f8a9dbb2efa41f9c4dd40ab33173b721ac
Naim Baziz <naim.baziz220@gmail.com>
2024-10-23 11:45:52 +0700

    temporary styling of search app

--- favorites/views.py
+++ favorites/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from favorites.models import Restaurant
+
 
 # Create your views here.
+def show_all_tuples(request):
+    tuples = Restaurant.objects.all()
+    return render(request, "templatetest.html", {"tuples": tuples})
