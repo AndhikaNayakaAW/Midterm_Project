4dd13af9c345efa2441c8d4fd57f255b86ef6c38
adyoprawira <adyoprawira@gmail.com>
2024-10-27 04:00:00 +0700

    Fixed login.html, explore button redirect

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '128': '32rem', // Custom width of 32rem (512px)
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
-
